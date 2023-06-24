189b808edbc181bbabd4044ec4783a6580cec1f1
javierIA <javier.flores@ia.center>
2023-06-24 10:36:14 -0500

    inclass

--- 24.junio.py
+++ 24.junio.py
@@ -6,6 +6,10 @@ def getcats():
     # https://dog.ceo/dog-api/breeds-list
     response = requests.get(url, stream=True)
     print(response.status_code)
+    if response.status_code == 200:
+        with open("cat.jpg", "wb") as f:
+            f.write(response.content)
+            
 
 
 if __name__ == "__main__":
